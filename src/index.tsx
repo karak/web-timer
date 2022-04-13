004752fa378c542f498563a36954dcc747b6f2e2
karak <karak97@gmail.com>
2022-04-13 09:50:41 +0900

    chore: added strong types.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import "./style.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 
-function toTwoDigits(n) {
+function toTwoDigits(n: number): string {
   return n.toFixed(0).padStart(2, "0");
 }
 
@@ -13,7 +13,7 @@ const INTERVAL = 1; // ms
  * Display time.
  * @param time: number - time in milliseconds
  */
-const TimerDisplay = ({ time }) => {
+const TimerDisplay: React.FC<{ time: number }> = ({ time }) => {
   const totalSeconds = Math.floor(time / 1000);
   const mm = toTwoDigits(Math.floor(totalSeconds / 60));
   const ss = toTwoDigits(totalSeconds % 60);
@@ -27,8 +27,15 @@ const TimerDisplay = ({ time }) => {
 
 const MAX_TIME = 3 * 60 * 1000; // three minutes
 
-class App extends React.Component {
-  constructor(props) {
+interface AppState {
+  startTime: number;
+  maxTime: number;
+  currentTime: number;
+}
+class App extends React.Component<{}, AppState> {
+  private intervalId: NodeJS.Timer | undefined;
+
+  constructor(props: {}) {
     super(props);
     const currentTime = new Date().getTime();
     this.state = {
@@ -46,7 +53,7 @@ class App extends React.Component {
     }, 1000);
   }
   componentWillUnmount() {
-    clearInterval(this.intervalId);
+    clearInterval(this.intervalId!);
   }
   render() {
     const { currentTime, startTime, maxTime } = this.state;

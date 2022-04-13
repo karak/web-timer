df5f2245b3b89757e1ffd9b2d05399d962c0cf86
karak <karak97@gmail.com>
2022-04-13 19:17:38 +0900

    refactor: formated.

--- src/__tests__/StopWatch.test.ts
+++ src/__tests__/StopWatch.test.ts
@@ -3,33 +3,43 @@ import StopWatch from "../StopWatch";
 const INTERVAL = 1000;
 
 describe("StopWatch", () => {
-    beforeAll(() => jest.useFakeTimers());
-    
-    afterAll(() => jest.useRealTimers());
+  beforeAll(() => jest.useFakeTimers());
 
-    it("should call callback after ticked", () =>{
-        const fn = jest.fn();
-        const stopWatch = new StopWatch(fn, INTERVAL);
-        stopWatch.start();
-        jest.advanceTimersByTime(INTERVAL);
-        expect(fn).toHaveBeenCalled();
-    });
+  afterAll(() => jest.useRealTimers());
 
-    it("should call callback after ticked less", () =>{
-        const fn = jest.fn();
-        const stopWatch = new StopWatch(fn, INTERVAL);
-        stopWatch.start();
-        jest.advanceTimersByTime(INTERVAL - 1);
-        expect(fn).not.toHaveBeenCalled();
-    });
+  it("should call callback after ticked", () => {
+    const fn = jest.fn();
+    const stopWatch = new StopWatch(fn, INTERVAL);
+    stopWatch.start();
+    jest.advanceTimersByTime(INTERVAL);
+    expect(fn).toHaveBeenCalled();
+  });
 
-    it("should call callback when stopped", () =>{
-        const fn = jest.fn();
-        const stopWatch = new StopWatch(fn, INTERVAL);
-        stopWatch.start();
-        jest.advanceTimersByTime(INTERVAL / 2);
-        stopWatch.stop();
-        jest.advanceTimersByTime(INTERVAL);
-        expect(fn).not.toHaveBeenCalled();
-    });
+  it("should call callback after ticked less", () => {
+    const fn = jest.fn();
+    const stopWatch = new StopWatch(fn, INTERVAL);
+    stopWatch.start();
+    jest.advanceTimersByTime(INTERVAL - 1);
+    expect(fn).not.toHaveBeenCalled();
+  });
+
+  it("shouldn't call callback when stopped", () => {
+    const fn = jest.fn();
+    const stopWatch = new StopWatch(fn, INTERVAL);
+    stopWatch.start();
+    jest.advanceTimersByTime(INTERVAL / 2);
+    stopWatch.stop();
+    jest.advanceTimersByTime(INTERVAL);
+    expect(fn).not.toHaveBeenCalled();
+  });
+
+  it("should call callback when restarted", () => {
+    const fn = jest.fn();
+    const stopWatch = new StopWatch(fn, INTERVAL);
+    stopWatch.start();
+    stopWatch.stop();
+    stopWatch.start();
+    jest.advanceTimersByTime(INTERVAL);
+    expect(fn).toHaveBeenCalled();
+  });
 });
